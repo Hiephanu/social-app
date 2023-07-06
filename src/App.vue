1576947a77ab2b3429c449c9b4abfc87b1c882fd
Hiephanu <doanvanhiep230903@gmail.com>
2023-07-06 22:04:34 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+
+const name =ref<string>('Hiêp')
+</script>
+
+<template>
+  <div>
+    {{ name }}
+  </div>
+</template>
+
---
+++ src/router/router.js
@@ -0,0 +1,8 @@
+import {createRouter,createWebHashHistory} from  'vue-router'
+
+
+const route=[
+    {path:'/',component:mainPage},
+    {path:'/aboutPage',component:aboutPage}
+]
+
