f0b42f547666c0df543b917ece182b5bd4683f84
hniht3002 <s2okaza2s@gmail.com>
2023-07-12 10:38:35 +0700

    Add image + video + fake account

--- src/views/user/Authen/loginPage.vue
+++ src/views/user/Authen/loginPage.vue
@@ -1,13 +1,5 @@
 <template>
     <div>
-        <Login class=""/>
-    </div>
-</template>
-<script setup lang="ts">
-import Login from '@/components/AuthenComponent/login/Login.vue';
-</script>
-<template>
-    <div>
         <Login/>
     </div>
 </template>
